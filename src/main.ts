4f24a3667a59e70e57eb03573ce0c846675f2336
gutaolox <32674347+gutaolox@users.noreply.github.com>
2021-04-05 03:07:55 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({ origin: 'http://localhost:3000' });
   const config = new DocumentBuilder()
     .setTitle('GG study Api')
     .setDescription('The GG Study API description')
